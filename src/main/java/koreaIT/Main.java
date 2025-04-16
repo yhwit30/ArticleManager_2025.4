e9c9009bd06455b9e687629b3b78aca375909ec5
yhwit30 <yhwit30@gmail.com>
2025-04-16 19:53:10 +0900

    추가 기능, 목록 기능 구현

--- src/main/java/koreaIT/Main.java
+++ src/main/java/koreaIT/Main.java
@@ -1,9 +1,12 @@
 package koreaIT;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        List<Article> articleList = new ArrayList<>();
 
         System.out.println("== 프로그램 시작 ==");
 
@@ -17,6 +20,9 @@ public class Main {
 
             if (cmd.equals("article list")){
                 System.out.println("번호  /  제목  / 내용");
+                for(int i = articleList.size()-1 ; i >= 0;i--){
+                    System.out.printf("%d   / %s    / %s\n", articleList.get(i).getId(), articleList.get(i).getTitle(),  articleList.get(i).getBody());
+                }
             }
             else if (cmd.equals("article write")){
                 System.out.print("제목 : ");
@@ -25,7 +31,16 @@ public class Main {
                 String body = sc.nextLine();
 
                 lastId++;
-                System.out.printf("%d번 글이 생성되었습니다.\n", lastId);
+                int id = lastId;
+
+                Article addArticle = new Article();
+                addArticle.setId(id);
+                addArticle.setTitle(title);
+                addArticle.setBody(body);
+
+                articleList.add(addArticle);
+
+                System.out.printf("%d번 글이 생성되었습니다.\n", id);
             }
 
             else if(cmd.equals("exit")){
@@ -37,10 +52,6 @@ public class Main {
             }
 
         }
-
-
-
-
 
     }
 }
