040e8326e63a543ae9b786165e328102a3a90bbf
yhwit30 <yhwit30@gmail.com>
2025-04-24 21:05:36 +0900

    게시글의 작성자만 수정 및 삭제가 가능하도록 구현

--- src/main/java/koreaIT/controller/ArticleController.java
+++ src/main/java/koreaIT/controller/ArticleController.java
@@ -60,10 +60,17 @@ public class ArticleController extends Controller {
         // parsing end
 
         // modifyId 로 게시글 찾아보기
-
         Article foundArticle = getArticleById(modifyId);
 
+
         if (foundArticle != null) {
+
+            // 로그인 중인 아이디와 게시글의 권한 체크
+            if (loginedMember.getId() != foundArticle.getMemberId()){
+                System.out.println("해당 게시글에 대한 권한이 없습니다.");
+                return;
+            }
+
             System.out.println("기존 제목 : " + foundArticle.getTitle());
             System.out.println("기존 내용 : " + foundArticle.getBody());
             System.out.print("새 제목 : ");
@@ -105,7 +112,13 @@ public class ArticleController extends Controller {
 
         // deleteId 로 게시글 찾아보기
         Article foundArticle = getArticleById(deleteId);
+
         if (foundArticle != null) {
+            // 로그인 중인 아이디와 게시글의 권한 체크
+            if (loginedMember.getId() != foundArticle.getMemberId()){
+                System.out.println("해당 게시글에 대한 권한이 없습니다.");
+                return;
+            }
             articleList.remove(foundArticle);
             System.out.printf("%d번 게시글이 삭제되었습니다.\n", deleteId);
         } else {
